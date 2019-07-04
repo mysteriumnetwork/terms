d8c406f24f59fad0159aa16679f52c6e508e7def
Mister CI tool <dev@mysterium.network>
2019-07-04 08:52:50 +0000

    Update terms packages [skip ci]

--- terms-go/terms_bindata.go
+++ terms-go/terms_bindata.go
@@ -93,7 +93,7 @@ func termsMd() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "TERMS.md", size: 16155, mode: os.FileMode(420), modTime: time.Unix(1562230299, 0)}
+	info := bindataFileInfo{name: "TERMS.md", size: 16155, mode: os.FileMode(420), modTime: time.Unix(1562230360, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
@@ -113,7 +113,7 @@ func warrantyMd() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "WARRANTY.md", size: 549, mode: os.FileMode(420), modTime: time.Unix(1562230299, 0)}
+	info := bindataFileInfo{name: "WARRANTY.md", size: 549, mode: os.FileMode(420), modTime: time.Unix(1562230360, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
