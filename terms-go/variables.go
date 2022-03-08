0cd01c13bd3f88482572417dfd7318bbf4dcd14c
Mantas Domaševičius <mantas@mysterium.network>
2022-03-08 13:31:01 +0200

    Change short terms filename

--- terms-go/variables.go
+++ terms-go/variables.go
@@ -1,10 +1,10 @@
 package terms
 
 var (
-	TermsBountyPilot   = MustAsset("TERMS_BOUNTY_PILOT.md")
-	TermsEndUser       = MustAsset("TERMS_END_USER.md")
-	TermsExitNode      = MustAsset("TERMS_EXIT_NODE.md")
-	TermsExitNodeShort = MustAsset("TERMS_EXIT_NODE_SHORT.md")
-	Warranty           = MustAsset("WARRANTY.md")
-	TermsVersion       = "0.1.0"
+	TermsBountyPilot = MustAsset("TERMS_BOUNTY_PILOT.md")
+	TermsEndUser     = MustAsset("TERMS_END_USER.md")
+	TermsExitNode    = MustAsset("TERMS_EXIT_NODE.md")
+	TermsNodeShort   = MustAsset("TERMS_NODE_SHORT.md")
+	Warranty         = MustAsset("WARRANTY.md")
+	TermsVersion     = "0.1.0"
 )
